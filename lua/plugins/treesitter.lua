21c0ea8368314f4d0edbc0841c6b925349d74157
Breno Barbosa de Oliveira <131599448+Brevex@users.noreply.github.com>
2024-10-06 23:06:08 -0300

    Add files via upload

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,44 @@
+return {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+
+    config = function()
+        require('nvim-treesitter.configs').setup {
+            
+            ensure_installed = { 
+                "c",
+		"cpp",
+		"cmake",
+		"css",
+		"dockerfile",
+		"go",
+		"html",
+		"java",
+		"javascript",
+		"kotlin",
+                "lua",
+		"python",
+		"rust",
+		"typescript",
+		"vue",
+                "vim", 
+                "vimdoc", 
+                "query", 
+                "markdown", 
+                "markdown_inline" 
+            },
+
+            sync_install = false,
+
+            auto_install = true,
+
+            highlight = {
+
+                enable = true,  
+
+                additional_vim_regex_highlighting = false,
+            },
+	}
+    end
+}
+
